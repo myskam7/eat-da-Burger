e3a61614daf95f48113dd9e4e426ab86cb44dc96
myskam7 <mksmflppv98@gmail.com>
2017-11-20 22:49:43 -0500

    update to jaws

--- server.js
+++ server.js
@@ -7,17 +7,17 @@ var PORT = process.env.PORT || 3000;
 var app = express();
 
 var mysql = require('mysql');
-var connection = mysql.createConnection(process.env.JAWSDB_URL);
+// var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
-connection.connect();
+// connection.connect();
 
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-  if (err) throw err;
+// connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+//   if (err) throw err;
 
-  console.log('The solution is: ', rows[0].solution);
-});
+//   console.log('The solution is: ', rows[0].solution);
+// });
 
-connection.end();
+// connection.end();
 
 
 // Serve static content for the app from the "public" directory in the application directory.
