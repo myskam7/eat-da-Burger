60376ba8cffb2de1c9096ce84cbf19d5af194557
myskam7 <mksmflppv98@gmail.com>
2017-11-20 22:51:31 -0500

    update to jaws

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 var connection;
 
-if(process.env.JAWSDB_ROSE_URL) {
+if(process.env.JAWSDB_WHITE_URL) {
   //Heroku deployment
-    connection = mysql.createConnection(process.env.JAWSDB_ROSE_URL);
+    connection = mysql.createConnection(process.env.JAWSDB_WHITE_URL);
 } else {
   connection = mysql.createConnection({
     root: 3306,
