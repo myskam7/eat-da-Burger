642e39f85c95b964c698a83878d55b3cd1726cf4
myskam7 <mksmflppv98@gmail.com>
2017-11-20 21:48:18 -0500

    eat-da-burger

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,17 @@
 var mysql = require('mysql');
-var connection = mysql.createConnection({
+var connection;
 
+if(process.env.JAWSDB_URL) {
+  //Heroku deployment
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
     database: "burgers_db"
   });
+};
 
 
 
